13c799c2b3f1115bcf47ed864641139085c0b16e
guohuanyang <guohuanyang@datagrand.com>
2020-11-03 16:00:44 +0800

    Add static files

--- app.py
+++ app.py
@@ -7,15 +7,28 @@
 # Date:         2020/11/3
 # Email         guohuanyang@datagrand.com
 # -------------------------------------------------------------------------------
-from flask import Flask
+from flask import Flask, render_template
 from flask import escape, url_for
 
 app = Flask(__name__)
 
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 @app.route('/')
-def hello():
-    return 'Welcome to my watchlist!!!'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -31,3 +44,4 @@ def test_url_for():
     print(url_for('user_page', name='chenjiani'))
     print(url_for('test_url_for', name=1))
     return 'test page'
+
