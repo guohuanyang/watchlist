dcdb84eb8b2bfa8445357c62b84c053e36cc4308
guohuanyang <244651200@qq.com>
2020-12-04 15:35:25 +0800

    add static files

--- app.py
+++ app.py
@@ -7,13 +7,26 @@
 # Date:         2020/11/3
 # Email         guohuanyang@datagrand.com
 # -------------------------------------------------------------------------------
+import os
+import sys
+
 from flask import Flask, render_template
 from flask import escape, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
+db = SQLAlchemy(app)
 
-name = 'Grey Li'
+tmp_name = 'Grey Li'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
@@ -26,9 +39,22 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    return render_template('index.html', name=tmp_name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -44,4 +70,3 @@ def test_url_for():
     print(url_for('user_page', name='chenjiani'))
     print(url_for('test_url_for', name=1))
     return 'test page'
-
